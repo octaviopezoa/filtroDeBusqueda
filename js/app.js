784ceeceb2ef668cebce0b3ca56559192762ad13
octavio pezoa <octaviopezoa@MacBook-Pro-de-octavio.local>
2022-03-29 22:03:55 -0300

    capturar valor de los select

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 // variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const color = document.querySelector('#color');
+const transmision = document.querySelector('#transmision');
+
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+const datosBusqueda = [{
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    color: '',
+    transmision: ''
+}];
 
 // eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -30,6 +47,30 @@ function mostrarAutos() {
 
     })
 }
+
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+})
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+})
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+
 
 function llenarSelect() {
     for (let i = max; i >= min; i--) {
